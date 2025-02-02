dafd6e7a3a30e2a9112e2de8b4483618b1b572e6
Mehroj Saparov <mehrojsaparov@Mehrojs-MacBook-Pro.local>
2025-02-02 19:14:08 +0500

    add?

---
+++ CSharpInMac/CSharpInMac/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpInMac;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var filePath = "../../../example.json";
+        if(File.Exists(filePath) is false)
+        {
+            File.WriteAllText(filePath, "[]");
+        }
+
+    }
+}
+
---
+++ CSharpInMac/CSharpInMac/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+
+namespace CSharpInMac;
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures) // 739
+    {
+        var res = new int[temperatures.Length];
+        for(var i = 0; i < temperatures.Length; i++)
+        {
+            var counter = 0;
+
+            for (var j = i; j < temperatures.Length; j++)
+            {
+                if (temperatures[i] > temperatures[j])
+                {
+                    counter++;
+                }
+                else
+                {
+                    res[i] = counter;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+
+
+    public bool Check(int[] nums) // 1768
+    {
+        var counter = 0;
+        int n = nums.Length;
+
+        for (var i = 0; i < n; i++)
+        {
+            if (nums[i] > nums[(i + 1) % n]) 
+            {
+                counter++;
+            }
+        }
+
+        return counter <= 1; 
+    }
+}
+
+
